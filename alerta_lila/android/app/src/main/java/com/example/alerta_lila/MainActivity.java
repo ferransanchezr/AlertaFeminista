d3430d8c7a58f74d6a2be71311d1aca7be29c667
ferransanchezr <ferran.sanchezr@e-campus.uab.cat>
2019-03-05 12:42:57 +0100

    project created

---
+++ alerta_lila/android/app/src/main/java/com/example/alerta_lila/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.alerta_lila;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
